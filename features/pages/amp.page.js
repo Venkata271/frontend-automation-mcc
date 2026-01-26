a5abf843bccd8f0bc5d5cf60a6e17c8490313854
Venkata271 <yvenkatasairam99@gmail.com>
2026-01-26 10:28:51 +0530

    Fix typo in iframe name for leadVideo locator

--- features/pages/amp.page.js
+++ features/pages/amp.page.js
@@ -1,7 +1,7 @@
 class AMPMediaArticlePage {
 constructor(page) {
 this.page = page;
-this.leadVideo = page.locator('iframe[name="amp_iframe1"]').contentFrame().locator('.cnx-ui-wrapper');
+this.leadVideo = page.locator('iframe[name="amp_ifram2e1"]').contentFrame().locator('.cnx-ui-wrapper');
 }
 }
 export default AMPMediaArticlePage;
